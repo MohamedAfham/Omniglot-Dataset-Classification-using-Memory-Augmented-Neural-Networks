d1f1440df00b288e44ef3ed405f197c57cf9acda
MohamedAfham <afhamaflal9@gmail.com>
2020-05-08 03:02:25 +0530

    Implemented fit_generator

--- classification.py
+++ classification.py
@@ -10,31 +10,34 @@ from MANN import MANNCell
 
 N = 5 #num_samples
 K = 2 #num_samples_per_class
-B = 100
+B = 10
 total = N*K*B
 manncell = MANNCell(rnn_size_list = 128, memory_size = 20,memory_vector_dim = 4,head_num =2)
 state = manncell.zero_state(total,tf.float32)
 read_vector_list = state['read_vector_list']
 
 data_generator = DataGenerator(num_classes = N,num_samples_per_class=K)
-train_data = data_generator.sample_batch('train',B)
-val_data = data_generator.sample_batch('val',25)
+#train_data = data_generator.sample_batch('train',B)
+#val_data = data_generator.sample_batch('val',25)
 inp = Input(shape=(784,))
 
-x = train_data[0]
-x_arr = [np.array(i) for i in x]
-x_tf = tf.constant(np.array(x_arr))
+#x = train_data[0]
+#x_arr = [np.array(i) for i in x]
+#model_input = tf.constant(np.array(x_arr))
 
-y_true = tf.constant(np.array([np.array(i) for i in train_data[1]]))
+#y_true = tf.constant(np.array([np.array(i) for i in train_data[1]]))
 
 y_pred,next_state = manncell(inp, state)
 state = next_state
-y_pred = tf.slice(y_pred,[0,0],[total,N])
+y_pred = tf.slice(tf.squeeze(y_pred),[0,0],[total,N])
 adam = tf.train.AdamOptimizer(learning_rate = 1.5)
 
 with tf.Session() as sess:  
     sess.run(tf.local_variables_initializer())
     sess.run(tf.global_variables_initializer())
     model = Model(inputs=inp,outputs=y_pred)
-    model.compile(optimizer = adam,loss = 'categorical_crossentropy')
+    model.compile(optimizer = adam,loss = 'categorical_crossentropy',metrics = ['accuracy'])
+    model.fit_generator(data_generator.sample_batch('train',B),epochs=1000,steps_per_epoch=10)
+
+    
         
