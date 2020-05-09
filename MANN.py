44797fc912e1b9e5b5c0598cb9ee4bd20669c514
MohamedAfham <afhamaflal9@gmail.com>
2020-05-10 02:32:15 +0530

    Image input sequence

--- MANN.py
+++ MANN.py
@@ -4,11 +4,12 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Input,LSTM,Reshape,Conv2D,Flatten
 
 class MANNCell():
-    def __init__(self, rnn_size,memory_size, memory_vector_dim, head_num, gamma=0.95,
+    def __init__(self, rnn_size_list,memory_size, memory_vector_dim, head_num,samples_per_batch, gamma=0.95,
                  reuse=True):
 
         #initialize all the variables
-        self.rnn_size_list = rnn_size
+        self.samples_per_batch = samples_per_batch
+        self.rnn_size_list = rnn_size_list
         self.memory_size = memory_size
         self.memory_vector_dim = memory_vector_dim
         self.head_num = head_num                                   
@@ -19,35 +20,35 @@ class MANNCell():
         
         #initialize controller as the basic rnn cell
         network = Sequential()
-        network.add(LSTM(units=rnn_size,activation='tanh',return_sequences=True))
-        #for unit in self.rnn_size_list:
-        #    network.add(LSTM(units=unit,activation='tanh',return_sequences=True))
+        #network.add(LSTM(units=rnn_size,activation='tanh',return_sequences=True))
+        for unit in self.rnn_size_list:
+            network.add(LSTM(units=unit,activation='tanh',return_sequences=True))
         #network.add(Flatten())
         #for layer in self.FC_layer_list:
         #    network.add(Dense(layer,activation='relu'))
-        network.add(LSTM(units=5,activation='tanh',return_sequences=True))
-        #network.add(Dense(5,activation='softmax'))
+        #network.add(LSTM(units=5,activation='tanh',return_sequences=True))
+        network.add(Dense(5,activation='softmax'))
 
         self.controller = network
 
     def __call__(self, x, prev_state):
-        prev_read_vector_list = prev_state['read_vector_list']   
+        prev_read_vector_list = prev_state['read_vector_list']
         controller_input = tf.concat([x] + prev_read_vector_list, axis=-1)
 
 
         #next we pass the controller, which is the RNN cell, the controller_input and prev_controller_state
-        controller_input = tf.expand_dims(controller_input,axis=1)
+        #controller_input = tf.expand_dims(controller_input,axis=1)
         controller_output = self.controller(controller_input)
-        controller_output = tf.squeeze(controller_output)
+        #controller_output = tf.squeeze(controller_output)
             
         num_parameters_per_head = self.memory_vector_dim + 1
         total_parameter_num = num_parameters_per_head * self.head_num
         
         #Initiliaze weight matrix and bias and compute the parameters
-        weights = tf.Variable(tf.random_normal([controller_output.get_shape()[1], total_parameter_num], stddev=0.35))
+        weights = tf.Variable(tf.random_normal([controller_output.get_shape()[0],controller_output.get_shape()[2], total_parameter_num], stddev=0.35))
         biases = tf.Variable(tf.zeros([total_parameter_num]))
         parameters = tf.nn.xw_plus_b(controller_output, weights, biases)
-        head_parameter_list = tf.split(parameters, self.head_num, axis=1)
+        head_parameter_list = tf.split(parameters, self.head_num, axis=2)
         
         
         #previous read weight vector
@@ -76,10 +77,10 @@ class MANNCell():
             with tf.variable_scope('addressing_head_%d' % i):
                 
                 #key vector
-                k = tf.tanh(head_parameter[:, 0:self.memory_vector_dim], name='k')
+                k = tf.tanh(head_parameter[:,:, 0:self.memory_vector_dim], name='k')
                 
                 #sig_alpha
-                sig_alpha = tf.sigmoid(head_parameter[:, -1:], name='sig_alpha')
+                sig_alpha = tf.sigmoid(head_parameter[:,:, -1:], name='sig_alpha')
                 
                 #read weights
                 w_r = self.read_head_addressing(k, prev_M)
@@ -96,7 +97,7 @@ class MANNCell():
         w_u = self.gamma * prev_w_u + tf.add_n(w_r_list) + tf.add_n(w_w_list)   
 
         #update the memory
-        M_ = prev_M * tf.expand_dims(1. - tf.one_hot(prev_indices[:, -1], self.memory_size), dim=2)
+        M_ = prev_M * tf.expand_dims(1. - tf.one_hot(prev_indices[:,:, -1], self.memory_size), dim=3)
         
         
         #write operation
@@ -104,20 +105,20 @@ class MANNCell():
         with tf.variable_scope('writing'):
             for i in range(self.head_num):
                 
-                w = tf.expand_dims(w_w_list[i], axis=2)
-                k = tf.expand_dims(k_list[i], axis=1)
+                w = tf.expand_dims(w_w_list[i], axis=3)
+                k = tf.expand_dims(k_list[i], axis=2)
                 M = M + tf.matmul(w, k)
 
         #read opearion
         read_vector_list = []
         with tf.variable_scope('reading'):
             for i in range(self.head_num):
-                read_vector = tf.reduce_sum(tf.expand_dims(w_r_list[i], dim=2) * M, axis=1)
+                read_vector = tf.reduce_sum(tf.expand_dims(w_r_list[i], dim=3) * M, axis=2)
                 read_vector_list.append(read_vector)       
 
         
         #controller output
-        NTM_output = tf.concat([controller_output] + read_vector_list, axis=1)
+        NTM_output = tf.concat([controller_output] + read_vector_list, axis=2)
 
         state = {
             'read_vector_list': read_vector_list,
@@ -135,10 +136,10 @@ class MANNCell():
         
         "content based cosine similarity"
         
-        k = tf.expand_dims(k, axis=2)
+        k = tf.expand_dims(k, axis=3)
         inner_product = tf.matmul(prev_M, k)
-        k_norm = tf.sqrt(tf.reduce_sum(tf.square(k), axis=1, keep_dims=True))
-        M_norm = tf.sqrt(tf.reduce_sum(tf.square(prev_M), axis=2, keep_dims=True))
+        k_norm = tf.sqrt(tf.reduce_sum(tf.square(k), axis=2, keep_dims=True))
+        M_norm = tf.sqrt(tf.reduce_sum(tf.square(prev_M), axis=3, keep_dims=True))
         norm_product = M_norm * k_norm
         K = tf.squeeze(inner_product / (norm_product + 1e-8))                  
         K_exp = tf.exp(K)
@@ -154,22 +155,22 @@ class MANNCell():
     #least used weight vector
     def least_used(self,w_u):
         _, indices = tf.nn.top_k(w_u, k=self.memory_size)
-        w_lu = tf.reduce_sum(tf.one_hot(indices[:, -self.head_num:], depth=self.memory_size), axis=1)
+        w_lu = tf.reduce_sum(tf.one_hot(indices[:,:, -self.head_num:], depth=self.memory_size), axis=2)
         return indices, w_lu
 
     
     #next we define the function called zero state for initializing all the states - 
     #controller state, read vector, weights and memory
     def zero_state(self,batch_size,dtype):
-        one_hot_weight_vector = np.zeros([batch_size, self.memory_size])
+        one_hot_weight_vector = np.zeros([batch_size,self.samples_per_batch, self.memory_size])
         one_hot_weight_vector[..., 0] = 1
         one_hot_weight_vector = tf.constant(one_hot_weight_vector, dtype=tf.float32)
         with tf.variable_scope('init', reuse=self.reuse):
             state = {
-                'read_vector_list': [tf.zeros([batch_size, self.memory_vector_dim])
+                'read_vector_list': [tf.zeros([batch_size,self.samples_per_batch, self.memory_vector_dim])
                                      for _ in range(self.head_num)],
                 'w_r_list': [one_hot_weight_vector for _ in range(self.head_num)],
                 'w_u': one_hot_weight_vector,
-                'M': tf.constant(np.ones([batch_size, self.memory_size, self.memory_vector_dim]) * 1e-6, dtype=tf.float32)
+                'M': tf.constant(np.ones([batch_size,self.samples_per_batch ,self.memory_size, self.memory_vector_dim]) * 1e-6, dtype=tf.float32)
             }
             return state
