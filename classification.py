2c31460f16a9cf8c02b120fd1986708bad632182
MohamedAfham <afhamaflal9@gmail.com>
2020-05-09 22:31:42 +0530

    Input Data shape change, concatenated the previous output

--- classification.py
+++ classification.py
@@ -12,13 +12,13 @@ N = 5 #num_samples
 K = 1 #num_samples_per_class
 B = 15
 total = N*K*B
-manncell = MANNCell(rnn_size_list = [512,256,128],FC_layer_list=[128,84], memory_size = 20,memory_vector_dim = 8,head_num =4)
+manncell = MANNCell(rnn_size = 128, memory_size = 20,memory_vector_dim = 4,head_num =2)
 state = manncell.zero_state(total,tf.float32)
 read_vector_list = state['read_vector_list']
 
 data_generator = DataGenerator(num_classes = N,num_samples_per_class=K)
 
-inp = Input(shape=(784,))
+inp = Input(shape=(784 + N,))
 
 y_pred,next_state = manncell(inp, state)
 state = next_state
@@ -30,7 +30,7 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     model = Model(inputs=inp,outputs=y_pred)
     model.compile(optimizer = adam,loss = 'categorical_crossentropy',metrics = ['accuracy'])
-    history = model.fit_generator(data_generator.sample_batch('train',B),epochs=40000,steps_per_epoch=50,validation_data = data_generator.sample_batch('val',B),validation_steps = B)
+    history = model.fit_generator(data_generator.sample_batch('train',B),epochs=50,steps_per_epoch=50,validation_data = data_generator.sample_batch('val',B),validation_steps = B)
     fig ,ax = plt.subplots(figsize=(8,4))
     ax.plot(history.history['accuracy'])
     ax.plot(history.history['val_accuracy'])
